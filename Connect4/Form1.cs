1cc76a23fd481eb8da4fa09f9eadff0364d05c84
MolnAtt <molnar.attila@szlgbp.hu>
2021-05-13 08:22:50 +0200

    Lenyomódtam.

--- Connect4/Form1.cs
+++ Connect4/Form1.cs
@@ -15,6 +15,12 @@ namespace Connect4
         public Form1()
         {
             InitializeComponent();
+            button2.Click += button1_Click;
+            button3.Click += button1_Click;
+            button4.Click += button1_Click;
+            button5.Click += button1_Click;
+            button6.Click += button1_Click;
+            button7.Click += button1_Click;
         }
 
         int kijön = 0;
@@ -30,6 +36,26 @@ namespace Connect4
                 label2.Text = "piros";
             }
             kijön = 1-kijön;
+
+            Button gomb = sender as Button;
+
+            gomb.Text = "lenyomódtam";
+
+            /*Ide az kéne, hogy ha valaki lenyomja a gombot, akkor 
+             a MEGNYOMOTT GOMB (gomb) alá terem egy golyó, elindul egy metronóm, 
+            a metronóm úsztatja lefele a golyót, és ha eléri a helyét, akkor megáll.
+             */
+
+
+            /*
+             * 1. részfeladat: ha megnyomsz egy gombot, induljon el egy kör, egy fix helyen
+             * 2. részfeladat, ha megnyomsz egy gombot, induljon el egy kör, egy fix helyen, de aztán álljon meg valahol.
+             * 3. részfeladat, ha megnyomsz egy gombot, induljon el egy kör, egy fix helyen, de aztán álljon meg valahol. De ha még egyszer megnyomod, akkor hamarabb álljon meg. legalább 6-szor működjön!
+             * 4. részfeladat, ha megnyomsz egy gombot, induljon el egy kör a megnyomott gomb alatt, de aztán álljon meg valahol. De ha még egyszer megnyomod, akkor hamarabb álljon meg. legalább 6-szor működjön!
+             * 5. részfeladat, ha megnyomsz egy gombot, induljon el egy kör a megnyomott gomb alatt, de aztán álljon meg valahol. De ha még egyszer megnyomod, akkor csak akkor álljon meg hamarabb, ha már van alatta valamit.
+             * 
+             */
+
         }
     }
 }
